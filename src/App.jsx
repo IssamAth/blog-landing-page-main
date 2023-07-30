20c52a6bbe1fbd9ceca0a5687c370d1df9c268be
Issam Athamena <samo.alger@gmail.com>
2023-07-30 01:52:25 +0100

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import LOGO from '../src/images/logo.svg';
 import IMG1 from '../src/images/illustration-editor-mobile.jpg';
 import IMG2 from '../src/images/illustration-editor-desktop.svg';
 import IMG3 from '../src/images/illustration-phones-cropped.svg';
+import IMG4 from '../src/images/illustration-laptop-desktop-cropped.svg';
+import IMG5 from '../src/images/illustration-laptop-mobile-cropped.svg';
 
 const getWidth = () => document.documentElement.clientWidth;
 
@@ -16,8 +18,24 @@ const ResponsiveImage = () => {
   }, []);
 
   return (
-    <div className=' md:w-55v'>
-      {width >= 600 ? <img src={IMG2} className='' alt="" /> : <img src={IMG1} alt="" />}
+    <div className=' md:w-55v '>
+      {width >= 767 ? <img src={IMG2} className='' alt="" /> : <img src={IMG1} alt="" />}
+    </div>
+  );
+};
+
+const ResponsiveImageTwo = () => {
+  const [width1, setWidth1] = useState(getWidth());
+
+  useEffect(() => {
+    const resize = () => setWidth1(getWidth());
+    window.addEventListener('resize', resize);
+    return () => window.removeEventListener('resize', resize);
+  }, []);
+
+  return (
+    <div className=''>
+      {width1 >= 767 ? <img src={IMG4} className='' alt="" /> : <img src={IMG5} alt="" />}
     </div>
   );
 };
@@ -84,19 +102,19 @@ const App = () => {
             <h2>Designed for the future</h2>
           </div>
           {/* FLEX ITEM */}
-          <div className='flex flex-col mt-10 md:flex-row-reverse md:relative md:items-center'>
+          <div className='flex flex-col mt-10 md:flex-row-reverse md:relative md:items-center md:w-full md:justify-between'>
             {/* IMAGE */}
-            <div className=' md:-mr-40 xl:-mr-72 lg:-mr-55 '>
+            <div className=' md:-mr-40 xl:-mr-85 lg:-mr-55 '>
               <ResponsiveImage />
             </div>
             {/* BOTH ARTICLES */}
-            <div className=' flex flex-col my-10 md:w-35v md:pr-14'>
+            <div className=' flex flex-col my-10 md:w-45v md:pr-0 lg:pr-14 xl:pr-0'>
               {/* FIRST ARTICLE */}
               <article className=' '>
                 <div className=' text-center text-2xlnHalf font-bold my-4 md:text-left md:text-2xl lg:text-2xlnHalf'>
                   <h3>Introducing an extensible editor</h3>
                 </div>
-                <div className=' text-center text-bigbase md:text-left md:text-bsm lg:text-bigbase'>
+                <div className=' text-center text-bigbase md:text-left md:text-sm xl:text-bigbase'>
                   <p>Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.</p>
                 </div>
               </article>
@@ -105,7 +123,7 @@ const App = () => {
                 <div className=' text-center text-2xlnHalf font-bold my-4 md:text-left md:text-2xl lg:text-2xlnHalf'>
                   <h3>Robust content management</h3>
                 </div>
-                <div className=' text-center text-bigbase md:text-left md:text-bsm lg:text-bigbase'>
+                <div className=' text-center text-bigbase md:text-left md:text-sm xl:text-bigbase'>
                   <p>Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
                 </div>
               </article>
@@ -134,6 +152,44 @@ const App = () => {
                 <p>With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
               </div>
             </article>
+          </div>
+        </div>
+      </section>
+
+
+      {/* FIFTH PART */}
+      <section className=' mt-16 mb-10 md:mt-28 md:overflow-hidden'>
+        {/* CONTAINER */}
+        <div className=' container mx-auto'>
+          {/* FLEX ITEM */}
+          <div className=' flex flex-col md:flex-row md:justify-between md:items-center md:h-full'>
+            {/* IMAGE PHONE */}
+            <div className=' md:w-60v mb-9 md:-ml-52 xl:-ml-98 md:mb-8'>
+              <ResponsiveImageTwo />
+            </div>
+            {/* BOTH ARTICLES */}
+            <div className=' flex flex-col md:w-40v xl:w-35v'>
+               {/* ARTICLE ONE */}
+              <article className=' mt-5 md:my-0'>
+                <div className=' text-center text-2xlnHalf font-bold mb-5 md:text-left md:text-xlnHalf xl:text-2xlnHalf'>
+                  <h2>Free, open, simple</h2>
+                </div>
+                {/* PARAGRAPH */}
+                <div className=' text-center md:text-left md:mb-0  md:text-bxs xl:text-base'>
+                  <p>Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.</p>
+                </div>
+              </article>
+              {/* ARTICLE TWO */}
+              <article className=' mt-9 md:mt-12'>
+                <div className=' text-center text-2xlnHalf font-bold mb-5 md:text-left md:text-xlnHalf xl:text-2xlnHalf'>
+                  <h2>Powerful tooling</h2>
+                </div>
+                {/* PARAGRAPH */}
+                <div className=' text-center md:text-left md:mb-0 md:text-bxs xl:text-base'>
+                  <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
+                </div>
+              </article>
+            </div>
           </div>
         </div>
       </section>
